65e98392255a1bb02497a028bd9c5d66728cada3
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-29 03:16:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -32,8 +32,8 @@ final_string = []
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do      
-      if src[row_index][element_index].class == String
-        final_string << src[row_index][element_index] + ' '
+      if src[row_index][element_index] == count.min
+        final_string << src[row_index][element_index].min
       end
       element_index += 1
     end
