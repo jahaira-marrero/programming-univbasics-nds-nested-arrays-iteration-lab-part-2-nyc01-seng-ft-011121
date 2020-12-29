02bd4af5572ccd624459b08bc6f6b0e9f02960b5
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-29 03:14:12 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,21 +9,35 @@ array_of_daily_temperatures = [
 ]
 
 # outer_results = []
-
+# def find_min_in_nested_arrays(src)
+#   # src will be an array of arrays of integers
+#   # Produce a new Array that contains the smallest number of each of the nested arrays
+# row_index = 0 
+# while row_index < src.count do
+#   element_index = 0 
+#   while element_index < src[row_index].count do
+#   if src[row_index][element_index].min?
+#   p src[row_index][element_index]
+# end
+# element_index += 1 
+# end
+# row_index += 1 
+# end
+# end
+# find_min_in_nested_arrays(array_of_daily_temperatures)
 
 def find_min_in_nested_arrays(src)
-  # src will be an array of arrays of integers
-  # Produce a new Array that contains the smallest number of each of the nested arrays
-row_index = 0 
-while row_index < src.count do
-  element_index = 0 
-  while element_index < src[row_index].count do
-  if src[row_index][element_index].min?
-  p src[row_index][element_index]
+final_string = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    while element_index < src[row_index].count do      
+      if src[row_index][element_index].class == String
+        final_string << src[row_index][element_index] + ' '
+      end
+      element_index += 1
+    end
+    row_index += 1
+  end
+  final_string
 end
-element_index += 1 
-end
-row_index += 1 
-end
-end
-find_min_in_nested_arrays(array_of_daily_temperatures)
